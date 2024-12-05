541f1b1f43f337682de915d91fb436469a884477
Ulaev Danila <ulaevdanila63@gmail.com>
2024-12-05 18:30:56 +0200

    Added BasePage.py

---
+++ pages/BasePage/BasePage.py
@@ -0,0 +1,10 @@
+class BasePage:
+    def __init__(self, browser):
+        self.browser = browser
+
+    def find_element(self, *args):
+        return self.browser.find_element(*args)
+
+    def find_elements(self, *args):
+        return self.browser.find_elements(*args)
+
